97a62b9853db60202a5f8ac7d90feeb955c14250
FizzingForWurf <zhtong315@gmail.com>
2022-11-03 22:09:03 +0800

    Abstract out and simplify turning operations

--- navigation.ino
+++ navigation.ino
@@ -1,38 +1,39 @@
 void move_forward() {
-  left_motor.run(-FORWARD_SPEED); 
-  right_motor.run(FORWARD_SPEED); 
+  left_motor.run(-FORWARD_SPEED);
+  right_motor.run(FORWARD_SPEED);
 }
 
 void move_forward_correction(int correction) {
-  left_motor.run(-FORWARD_SPEED - correction); 
-  right_motor.run(FORWARD_SPEED - RIGHT_DEVIATION - correction); // Slow down right wheel
+  left_motor.run(-FORWARD_SPEED + LEFT_DEVIATION - correction);
+  right_motor.run(FORWARD_SPEED - RIGHT_DEVIATION - correction);
 }
 
 void turn_left() {
-  left_motor.run(TURN_SPEED); 
-  right_motor.run(TURN_SPEED); 
+  left_motor.run(TURN_SPEED);
+  right_motor.run(TURN_SPEED);
 }
 
 void turn_right() {
-  left_motor.run(-TURN_SPEED); 
-  right_motor.run(-TURN_SPEED); 
+  left_motor.run(-TURN_SPEED);
+  right_motor.run(-TURN_SPEED);
 }
 
 void stop_moving() {
-  left_motor.stop(); 
-  right_motor.stop(); 
+  left_motor.stop();
+  right_motor.stop();
 }
 
 float calculate_pid() {
   float wall_dist = get_ultrasonic_distance();
   float filtered_dist = apply_low_pass_filter(wall_dist);
-  
-  if (wall_dist == 0.0 or wall_dist > PID_SETPOINT*1.5) {
+
+  // If wall is not present, return 0
+  if (wall_dist == 0.0 or wall_dist > PID_SETPOINT * 1.5) {
     pid_i_mem = 0.0; prev_pid_error = 0.0;
     return 0.0;
   }
-  
-  float pid_error = filtered_dist - PID_SETPOINT;
+
+  float pid_error = filtered_dist - PID_SETPOINT; // Calculate PID error
   pid_i_mem += PID_I_GAIN * pid_error;
   if (pid_i_mem > PID_MAX_I) pid_i_mem = PID_MAX_I;
   else if (pid_i_mem < PID_MAX_I * -1) pid_i_mem = PID_MAX_I * -1;
@@ -50,30 +51,68 @@ float calculate_pid() {
   return pid_output;
 }
 
-float prev_dist = 0.0, prev_prev_dist = 0.0;
-bool is_close_to_wall(int state) {
-  float lower, upper;
-  
-  if (state == TURN_RIGHT or state == TWO_RIGHT) {
-    lower = 6.0; upper = 7.0; 
-  } else {
-    lower = 9.5; upper = 10.5;
-  }
+void turn_left_time(int duration) {
+  turn_left();
+  delay(duration);
 
-  int counter = 0;
-  float reading = get_ultrasonic_distance();
-  
-  if (reading > lower and reading < upper) counter++;
-  if (prev_dist > lower and prev_dist < upper) counter++;
-//  if (prev_prev_dist > lower and prev_prev_dist < upper) counter++;
+  stop_moving();
+  global_state = FORWARD;
 
-  prev_prev_dist = prev_dist;
-  prev_dist = reading;
-
-#ifndef DEBUG_DETECT_WALL
-  if (counter == 2) Serial.println("WALL DETECTED!");
-  else Serial.println("NO WALL");
-#endif
-
-  return counter == 2;
 }
+
+void turn_right_time(int duration) {
+  turn_right();
+  delay(duration);
+
+  stop_moving();
+  global_state = FORWARD;
+}
+
+void compound_turn_left() {
+  // First turn
+  turn_left();
+  delay(TURN_LEFT_TIME_MS);
+
+  // Move forward
+  stop_moving();
+  move_forward();
+  delay(STRAIGHT_LEFT_TIME_MS);
+  stop_moving();
+
+  // Second turn
+  turn_left();
+  delay(TURN_LEFT_TIME_MS);
+
+  stop_moving();
+  global_state = FORWARD;
+}
+
+void compound_turn_right() {
+  // First turn
+  turn_right();
+  delay(TURN_RIGHT_TIME_MS);
+
+  // Move forward
+  stop_moving();
+  move_forward();
+  delay(STRAIGHT_RIGHT_TIME_MS);
+  stop_moving();
+  
+  // Second turn
+  turn_right();
+  delay(TURN_RIGHT_TIME_MS);
+
+  stop_moving();
+  global_state = FORWARD;
+}
+
+// Can consider using???
+//void extra_turn_correction() {
+//  if (get_ultrasonic_distance() < PID_SETPOINT * 1.5) 
+//    return; // Wall detected!
+//
+//  checkpoint_sound();
+//  turn_right(); // Turn RIGHT for ultrasonic to be closer to wall
+//  delay(EXTRA_TURN_TIME_MS);
+//  stop_moving();
+//}
--- project.ino
+++ project.ino
@@ -3,26 +3,21 @@
 
 #define DEBUG_ULTRASONIC
 #define DEBUG_FILTER
-#define DEBUG_DETECT_WALL
 #define DEBUG_INFRARED
 #define DEBUG_COLOR
 #define DEBUG_PID
 
-#define FORWARD_SPEED 200 
-#define RIGHT_DEVIATION 20 // Slow down right wheel for ultrasonic to detect wall
+#define FORWARD_SPEED 180 
+#define RIGHT_DEVIATION 0 
+#define LEFT_DEVIATION 0 //15
 
 #define TURN_SPEED 125
-#define TURN_TIME_MS 700
-#define UTURN_TIME_MS 1250
-#define STRAIGHT_TIME_MS 1000
-
-#define WHITE 0
-#define BLACK 1
-#define GREY 2
-
-#define R 0
-#define G 1
-#define B 2
+#define TURN_RIGHT_TIME_MS 625
+#define TURN_LEFT_TIME_MS 625
+#define UTURN_TIME_MS 1150
+#define STRAIGHT_RIGHT_TIME_MS 900
+#define STRAIGHT_LEFT_TIME_MS 1000
+#define EXTRA_TURN_TIME_MS 100
 
 #define D1 A2
 #define D2 A3
@@ -33,13 +28,16 @@ MeDCMotor left_motor(M1);
 MeDCMotor right_motor(M2);
 MeLineFollower line_finder(PORT_2);
 
-float PID_P_GAIN = 30.0;
-float PID_I_GAIN = 0.0;
-float PID_D_GAIN = 150.0;
+const char R = 0, G = 1, B = 2;
+const char white = 0, black = 1, grey = 2;
 
-float PID_SETPOINT = 10.0;
-float PID_MAX_I = 15.0;
-float PID_MAX_OUTPUT = 50.0;
+const float PID_P_GAIN = 30.0;
+const float PID_I_GAIN = 0.0;
+const float PID_D_GAIN = 800.0;
+
+const float PID_SETPOINT = 10.0;
+const float PID_MAX_I = 15.0;
+const float PID_MAX_OUTPUT = 60.0;
 
 float pid_i_mem = 0.0, prev_pid_error = 0.0;
 
@@ -66,14 +64,7 @@ enum Color {
 enum Motion {
   TURN_LEFT, TURN_RIGHT, U_TURN, TWO_LEFT, TWO_RIGHT, CHALLENGE, FORWARD
 };
-Motion state = FORWARD;
-
-long start_turn_time = 0;
-
-enum CompoundTurnStage {
-  FIRST_TURN, STRAIGHT, SECOND_TURN
-};
-CompoundTurnStage turn_stage = FIRST_TURN;
+Motion global_state = FORWARD;
 
 void setup() {
   Serial.begin(9600);
@@ -84,113 +75,45 @@ void setup() {
 
   read_eeprom();
 
-  //  celebrate();
   delay(1000);
-  start_turn_time = millis();
 }
 
 void loop() {
-  if (state == FORWARD) {
+  if (global_state == FORWARD) {
     float correction = calculate_pid();
     move_forward_correction((int) correction);
 
-    if (has_reached_waypoint()) {
-      stop_moving();
-      state = CHALLENGE;
-    }
+//    if (has_reached_waypoint()) {
+//      stop_moving();
+//      global_state = CHALLENGE;
+//    }
 
-  } else if (state == CHALLENGE) {
+  } else if (global_state == CHALLENGE) {
     read_color_sensor(); // Updates currentColor[3] with R,G,B values
     int predicted_color = match_color();
-
-    read_color_sensor(); // Updates currentColor[3] with R,G,B values
-    int second_color = match_color();
-
-    if (predicted_color != second_color) {
-      read_color_sensor(); // Updates currentColor[3] with R,G,B values
-      predicted_color = match_color();
-    }
-
-    //    read_color_sensor(); // Updates currentColor[3] with R,G,B values
-    //    int predicted_color = match_color();
-
     display_color(predicted_color);
-    //    checkpoint_sound();
 
-    if (predicted_color == C_WHITE) {
-      stop_moving();
-      celebrate();
-    } else {
-      state = static_cast<Motion>(predicted_color);
-      start_turn_time = millis(); // Set start time of turn
-      turn_stage == FIRST_TURN; // Reset turn stage to do first turn for compound turn
-    }
+//    if (predicted_color == C_WHITE) {
+//      stop_moving();
+//      celebrate();
+//      global_state = 10; // Terminates program with undefined state
+//
+//    } else global_state = static_cast<Motion>(predicted_color);
 
-  } else if (state == TURN_LEFT or state == TURN_RIGHT) {
-    bool within_time = millis() - start_turn_time < TURN_TIME_MS;
-    bool not_close_to_wall = (millis() - start_turn_time > (float) TURN_TIME_MS * 0.5) and not is_close_to_wall(state);
+  } else if (global_state == TURN_LEFT) {
+    turn_left_time(TURN_LEFT_TIME_MS);
 
-    if (within_time and true) {
-      if (state == TURN_LEFT) turn_left();
-      else if (state == TURN_RIGHT) turn_right();
+  } else if (global_state == TURN_RIGHT) {
+    turn_right_time(TURN_RIGHT_TIME_MS);
+    
+  } else if (global_state == U_TURN) {
+    turn_left_time(UTURN_TIME_MS);  // Turn LEFT for U-turn
 
-    } else {
-      stop_moving();
-      state = FORWARD;
-    }
-
-  } else if (state == U_TURN) {
-    bool within_time = millis() - start_turn_time < UTURN_TIME_MS;
-    bool not_close_to_wall = (millis() - start_turn_time > (TURN_TIME_MS * 0.75)) and not is_close_to_wall(state);
-
-    if (within_time and true)
-      turn_left();
-    else {
-      stop_moving();
-      state = FORWARD;
-    }
-
-  } else if (state == TWO_LEFT or state == TWO_RIGHT) {
-    if (turn_stage == FIRST_TURN) {
-      bool within_time = millis() - start_turn_time < TURN_TIME_MS;
-      //      bool not_close_to_wall = (millis() - start_turn_time > (TURN_TIME_MS*0.5)) and not is_close_to_wall(state);
-
-      if (within_time and true) {
-        if (state == TWO_LEFT) turn_left();
-        else if (state == TWO_RIGHT) turn_right();
-
-      } else {
-        stop_moving();
-        turn_stage = STRAIGHT;
-        start_turn_time = millis();
-      }
-
-    } else if (turn_stage == STRAIGHT) {
-      if (millis() - start_turn_time < STRAIGHT_TIME_MS) {
-        move_forward();
-        //        float correction = calculate_pid();
-        //        move_forward_correction((int) correction);
-
-      } else {
-        stop_moving();
-        turn_stage = SECOND_TURN;
-        start_turn_time = millis();
-      }
-
-    } else if (turn_stage == SECOND_TURN) {
-      bool within_time = millis() - start_turn_time < TURN_TIME_MS;
-
-      bool not_close_to_wall = (millis() - start_turn_time > (TURN_TIME_MS * 0.5)) and not is_close_to_wall(state);
-
-      if (within_time and true) {
-        if (state == TWO_LEFT) turn_left();
-        else if (state == TWO_RIGHT) turn_right();
-
-      } else {
-        stop_moving();
-        state = FORWARD;
-      }
-    }
+  } else if (global_state == TWO_LEFT) {
+    compound_turn_left();
+    
+  } else if (global_state == TWO_RIGHT) {
+    compound_turn_right();
   }
 
   delay(10);
